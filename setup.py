99ee38f6af1490744ae6c1701b2abfb9e92022fd
Gleb Pospelov <codefo@codefo.com>
2017-11-02 21:24:40 +0300

    Update package

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+
+base_dir = os.path.dirname(__file__)
+
+
+with open(os.path.join(base_dir, "README.rst")) as f:
+    long_description = f.read()
 
 
 setup(
   name='pymobius',
   packages=['pymobius'],
-  version='0.1.0',
+  version='1.0.0',
   description='Mobius Python API Client',
+  long_description=long_description,
+  include_package_data=True,
+  zip_safe=False,
   author='mobius-network',
   author_email='support@mobius.network',
   url='https://github.com/mobius-network/mobius-python',
